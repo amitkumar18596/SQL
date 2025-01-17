463e84ef91cdb96d047ddd17404563b95c33f4e1
amitkumar18596 <maharana.amit96@gmail.com>
2025-01-17 14:08:21 +0530

    Day 8 of solving SQL50 on Leetcode

---
+++ Leetcode_50_Days/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/submissions/1511314418/?envType=study-plan-v2&envId=top-sql-50
+# Write your MySQL query statement below
+select
+    v.customer_id, count(v.visit_id) as count_no_trans
+from
+    Visits v
+left join 
+    Transactions t
+on
+    v.visit_id=t.visit_id
+where t.transaction_id is null
+group by v.customer_id;
