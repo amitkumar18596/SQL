5d61a1b39e6d2d1c483a7ca9c528a41b0675fdcf
amitkumar18596 <maharana.amit96@gmail.com>
2025-01-26 00:29:37 +0530

    Day 17 of solving SQL50 on Leetcode

---
+++ Leetcode_50_Days/Employees Whose Manager Left the Company.sql
@@ -0,0 +1,5 @@
+#https://leetcode.com/problems/employees-whose-manager-left-the-company/description/?envType=study-plan-v2&envId=top-sql-50
+# Write your MySQL query statement below
+select  employee_id from Employees 
+where salary < 30000 and manager_id not in (select employee_id from Employees)
+order by employee_id;
